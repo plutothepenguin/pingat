a54b534b30f01b4199f4fb96424765a7ecc1d13b
omidghafori <cheeesebrain@gmail.com>
2018-11-17 21:39:56 -0500

    added chat

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var messages = [];
 
 app.get('/', function(req, res) {
-  res.send('home page');
+  res.render('home.ejs', {messages: messages});
 })
 app.listen(process.env.PORT || 3000, function() {
   console.log('started');
