1c24cd73144e0843154cbf4684b1125b78e03ff5
omidghafori <cheeesebrain@gmail.com>
2018-11-23 12:52:25 -0500

    added settings

--- main.js
+++ main.js
@@ -1,89 +1,193 @@
-var messages = [{
-  user: "Bob",
-  avatar: "profile",
-  message: "Hi"
-},{
+// var messages = [{
+//   user: "Bob",
+//   message: "Hi"
+// },{
+//   user: "Jeff",
+//   message: "Hello"
+// },{
+//   user: "Bob",
+//   message: "how you doing"
+// },{
+//   user: "Jeff",
+//   message: "heckin good"
+// },{
+//   user: "Bob",
+//   message: "im doing gooder"
+// },{
+//   user: "Jeff",
+//   message: "shut up nerd"
+// },{
+//   user: "Bob",
+//   message: "ok"
+// },{
+//   user: "Bob",
+//   message: "Hi"
+// },{
+//   user: "Jeff",
+//   message: "Hello"
+// },{
+//   user: "Bob",
+//   message: "how you doing"
+// },{
+//   user: "Jeff",
+//   message: "heckin good"
+// },{
+//   user: "Bob",
+//   message: "im doing gooder"
+// },{
+//   user: "Jeff",
+//   message: "shut up nerd"
+// },{
+//   user: "Bob",
+//   message: "ok"
+// },{
+//   user: "Bob",
+//   message: "Hi"
+// },{
+//   user: "Jeff",
+//   message: "Hello"
+// },{
+//   user: "Bob",
+//   message: "how you doing"
+// },{
+//   user: "Jeff",
+//   message: "heckin good"
+// },{
+//   user: "Bob",
+//   message: "im doing gooder"
+// },{
+//   user: "Jeff",
+//   message: "shut up nerd"
+// },{
+//   user: "Bob",
+//   message: "ok"
+// }]
+var friends = [{
   user: "Jeff",
-  avatar: "profile",
-  message: "Hello"
+  messages: [{
+    user: "Bob",
+    message: "Hi"
+  },{
+    user: "Jeff",
+    message: "Hello"
+  },{
+    user: "Bob",
+    message: "how you doing"
+  },{
+    user: "Jeff",
+    message: "heckin good"
+  },{
+    user: "Bob",
+    message: "im doing gooder"
+  },{
+    user: "Jeff",
+    message: "shut up nerd"
+  },{
+    user: "Bob",
+    message: "ok"
+  },{
+    user: "Bob",
+    message: "Hi"
+  },{
+    user: "Jeff",
+    message: "Hello"
+  },{
+    user: "Bob",
+    message: "how you doing"
+  },{
+    user: "Jeff",
+    message: "heckin good"
+  },{
+    user: "Bob",
+    message: "im doing gooder"
+  },{
+    user: "Jeff",
+    message: "shut up nerd"
+  },{
+    user: "Bob",
+    message: "ok"
+  },{
+    user: "Bob",
+    message: "Hi"
+  },{
+    user: "Jeff",
+    message: "Hello"
+  },{
+    user: "Bob",
+    message: "how you doing"
+  },{
+    user: "Jeff",
+    message: "heckin good"
+  },{
+    user: "Bob",
+    message: "im doing gooder"
+  },{
+    user: "Jeff",
+    message: "shut up nerd"
+  },{
+    user: "Bob",
+    message: "ok"
+  }]
 },{
   user: "Bob",
-  avatar: "profile",
-  message: "how you doing"
+  messages: [{
+    user: "Bob",
+    message: "heck"
+  }]
 },{
-  user: "Jeff",
-  avatar: "profile",
-  message: "heckin good"
+  user: "Jeffica",
+  messages: [{
+    user: "Bob",
+    message: "test"
+  },{
+    user: "Jeffica",
+    message: "yo"
+  }]
 },{
-  user: "Bob",
-  avatar: "profile",
-  message: "im doing gooder"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "shut up nerd"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "ok"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "Hi"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "Hello"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "how you doing"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "heckin good"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "im doing gooder"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "shut up nerd"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "ok"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "Hi"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "Hello"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "how you doing"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "heckin good"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "im doing gooder"
-},{
-  user: "Jeff",
-  avatar: "profile",
-  message: "shut up nerd"
-},{
-  user: "Bob",
-  avatar: "profile",
-  message: "ok"
+  user: "Bobita",
+  messages: [{
+    user: "Bobita",
+    message: "breh"
+  }]
 }]
+var page = 'Jeff';
+// friends.find(x => x.user === page).length;
 
-for (var i = 0; i < messages.length; i++) {
-  $('.messages-container').append('<div class="message"><div class="avatar" title="'+messages[i].user+'"><img src="'+messages[i].avatar+'.png" alt="'+messages[i].user+'"></div><div class="message-text">'+messages[i].message+'</div></div>')
+var messages = friends.find(x => x.user === page).messages;
+$(document).ready(function() {
+  $('.messages-container').scrollTop($('.messages-container')[0].scrollHeight);
+})
+for (var i = 0; i < friends.length; i++) {
+  $('.friends-container').append('<div class="friend"><div class="avatar-small"><img class="friend-avatar" src="' + friends[i].user +'.png" alt="' + friends[i].user +'"><div class="friend-text">' + friends[i].user + '</div></div></div>')
 }
+function start() {
+  messages = friends.find(x => x.user === page).messages;
+  $('.messages-container').empty();
+  $('.messages-header').text('@' + page);
+  for (var i = 0; i < friends.find(x => x.user === page).messages.length; i++) {
+    $('.messages-container').append('<div class="message"><div class="avatar" title="'+messages[i].user+'"><img class="message-avatar" src="'+messages[i].user+'.png" alt="'+messages[i].user+'"></div><div class="message-text">'+messages[i].message+'</div></div>')
+  }
+  $('.messages-container').scrollTop($('.messages-container')[0].scrollHeight);
+}
+
+$('.settings-button').click(function() {
+  $('.settings-overlay').css('bottom', '0');
+})
+$('.settings-close').click(function() {
+  $('.settings-overlay').css('bottom', '100vh');
+})
+$('.toggleable').click(function() {
+  $('.ball').toggleClass('on');
+  if ($('.ball').hasClass('on')) {
+    $('.toggleable').css('background-color', '#00ACBF');
+  }
+  else {
+    $('.toggleable').css('background-color', 'white');
+  }
+})
+
+start();
+$('.friend').click(function() {
+  page = $(this).text();
+  start();
+});
